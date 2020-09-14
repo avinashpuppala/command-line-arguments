8e34c4a7d740d636c4ce8ec0103d7760145c1e71
PUPPALA AVINASH <201810100580@presidencyuniversity.in>
2020-09-14 13:42:42 +0530

    Add project files.

---
+++ command line arguments/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace command_line_arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("no of command line arguements:" +arg);
+                foreach (string arg in args)
+                Console.WriteLine("arg");
+            Console.ReadLine();
+        }
+    }
+}
